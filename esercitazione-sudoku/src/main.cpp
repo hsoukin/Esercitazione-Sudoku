b736e8248dc3761ae8fc6d48036650a8f0597ed7
hsoukin <59317884+hsoukin@users.noreply.github.com>
2020-06-04 16:16:23 +0200

    Add files via upload

---
+++ esercitazione-sudoku/src/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <random>
+#include <time.h>
+
+// L'inclusione di ulteriori librerie è permesso,
+// sebbene non strettamente necessario.
+
+using namespace std;
+
+int main()
+{
+    srand(time(NULL)); // inizializzazione della funzione rand()
+    
+    // INSERIRE QUI IL CODICE
+    
+    return 0;
+}
